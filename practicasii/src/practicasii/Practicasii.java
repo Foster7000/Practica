668b0a94aa7403ff0ce88771b73935001408f516
Foster7000 <dani70granada@gmail.com>
2023-05-05 20:31:12 +0200

    cambiando codigo

--- practicasii/src/practicasii/Practicasii.java
+++ practicasii/src/practicasii/Practicasii.java
@@ -7,6 +7,7 @@ public class Practicasii {
   
     public static void main(String[] args) {
         System.out.println("HOLA BUENAS TARDES");
+        System.out.println("QUE TAL ESTAMOS");
     }
     
 }
