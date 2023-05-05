86bc791429ef401ffc549145b04a1f3208d4ad05
MiguelMataRamos <131800578+MiguelMataRamos@users.noreply.github.com>
2023-05-05 20:33:38 +0200

    Update Practicasii.java

--- practicasii/src/practicasii/Practicasii.java
+++ practicasii/src/practicasii/Practicasii.java
@@ -8,6 +8,8 @@ public class Practicasii {
     public static void main(String[] args) {
         System.out.println("HOLA BUENAS TARDES");
         System.out.println("QUE TAL ESTAMOS");
+        System.out.println("PUES AQUI ESTAMOS COMPAÑERO");
+        System.out.println("NO NOS PODEMOS QUEJAR");
     }
     
 }
